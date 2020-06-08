2d185b0651fa2a433723a2b4daf9e2b142971d6f
Amandeep Singh <amandeep11121993@gmail.com>
2020-06-08 18:55:26 +0530

    recursive cutting a rod

--- dynamicProgramming/cuttingARod/recursion.go
+++ dynamicProgramming/cuttingARod/recursion.go
@@ -4,7 +4,7 @@ import "fmt"
 
 var maxPrice int
 
-func findMaxPrice(length int, prices []int, index int, currentPrice int) {
+func findMaxPrice(length int, prices []int, currentPrice int) {
 	if length < 0 {
 		return
 	}
@@ -12,20 +12,18 @@ func findMaxPrice(length int, prices []int, index int, currentPrice int) {
 		maxPrice = currentPrice
 	}
 
-	for i := index; i < len(prices); i++ {
-		findMaxPrice(length-(i+1), prices, i+1, currentPrice+prices[i])
-		findMaxPrice(length-(i+1), prices, i, currentPrice+prices[i])
-		findMaxPrice(length, prices, i+1, currentPrice)
+	for i := 0; i < len(prices); i++ {
+		findMaxPrice(length-(i+1), prices, currentPrice+prices[i])
 	}
 }
 
 func main() {
 
 	length := 8
-	// prices := []int{1, 5, 8, 9, 10, 17, 17, 20}
-	prices := []int{3, 5, 8, 9, 10, 17, 17, 20}
+	prices := []int{1, 5, 8, 9, 10, 17, 17, 20}
+	// prices := []int{3, 5, 8, 9, 10, 17, 17, 20}
 
-	findMaxPrice(length, prices, 0, 0)
+	findMaxPrice(length, prices, 0)
 
 	fmt.Println(maxPrice)
 }
