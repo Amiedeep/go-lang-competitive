60499e0df05c45b6240c13a2713b3b988fa750bd
Amandeep Singh <amandeep11121993@gmail.com>
2020-07-18 18:35:51 +0530

    2d int slice utils

--- utils/utils.go
+++ utils/utils.go
@@ -9,9 +9,28 @@ func PrintSlice(input []int) {
 	}
 }
 
+func Print2DSlice(input [][]int) {
+	for _, row := range input {
+		for _, column := range row {
+			fmt.Printf("%d  ", column)
+		}
+		fmt.Println()
+	}
+}
+
 func Max(x, y int) int {
 	if x < y {
 		return y
 	}
 	return x
+}
+
+// Create2DIntSlice creates and returns a 2d slice.
+func Create2DIntSlice(length int) [][]int {
+	slice := make([][]int, length)
+	for i := range slice {
+		slice[i] = make([]int, length)
+	}
+
+	return slice
 }
